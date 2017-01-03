153b6c111d7fa67de43f8d11329f4d7a8fb8b289
Ford <Kenneth.Ford@capitalone.com>
2017-01-03 16:22:54 -0500

    Finished 10th challenge in introductions

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -18,6 +18,8 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> { // can be converted to {x, y -> y-x}
+        override fun compare(x: Int, y: Int) = y - x
+    })
     return arrayList
 }
--- src/i_introduction/_2_Named_Arguments/NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/NamedArguments.kt
@@ -22,16 +22,5 @@ fun todoTask2(): Nothing = TODO(
     references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
 
 fun task2(collection: Collection<Int>): String {
-    val sb = StringBuilder()
-    sb.append("{")
-    val iterator = collection.iterator()
-    while (iterator.hasNext()) {
-        val element = iterator.next()
-        sb.append(element)
-        if (iterator.hasNext()) {
-            sb.append(", ")
-        }
-    }
-    sb.append("}")
-    return collection.joinToString()
+    return collection.joinToString(prefix = "{", postfix = "}");
 }
